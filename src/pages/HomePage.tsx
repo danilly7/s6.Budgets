d63e2ab5ec14727b3df1269fb0851e7b89b8be62
Danilly Condori <danilly7@gmail.com>
2024-12-09 19:23:13 +0100

    Readme updated

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -10,7 +10,7 @@ export const HomePage = () => {
             <div className="relative z-10 flex items-center justify-center h-full">
                 <div className="bg-white p-8 rounded-lg shadow-lg max-w-xl text-pretty mx-auto">
                     <h1 className="text-teal-950 text-3xl sm:text-4xl font-bold mb-10 text-center">Benvingut a Pressupostos DC</h1>
-                    <p className="text-base sm:text-lg">En Pressupostos DC, t'ajudem a crear pressupostos per a la teva pàgina web de manera ràpida i fàcil.</p>
+                    <p className="text-base sm:text-lg">En Pressupostos DC, t'ajudem a crear pressupostos per a la teva pàgina web de manera fàcil i ràpida.</p>
                     <p className="text-base sm:text-lg">Només has de seleccionar les opcions que necessitis, com SEO, publicitat o disseny web, i obtindràs un pressupost personalitzat.</p>
                     <p className="text-base sm:text-lg">A més, podràs guardar-ho i associar-ho al teu correu electrònic per a accedir a ell quan tu vulguis.</p>
                     <h2 className="text-teal-950 text-lg sm:text-xl font-semibold mt-10 text-center">Crea el teu pressupost avui mateix!</h2>
